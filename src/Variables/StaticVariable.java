e09b501f89f177611af7e8ea771ee1473e335db1
bhavani <bhavani.cs21@bitsathy.ac.in>
2025-02-05 17:41:09 +0530

    first commit

---
+++ src/Variables/StaticVariable.java
@@ -0,0 +1,19 @@
+package Variables;
+class Student{
+    //static variable
+    static int age;
+}
+
+public class StaticVariable {
+    public static void main(String args[]){
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s1.age = 24;
+        s2.age = 21;
+        Student.age = 23;
+        Student.age = 25;
+
+        System.out.println("S1\'s age is: " + s1.age);
+        System.out.println("S2\'s age is: " + s2.age);
+    }
+}
